d3f604bf1059bd0697f87307c006901ebd4bf95d
Leigh Michael Forrest <leigh@catbirdseat.io>
2023-08-13 18:48:19 -0400

    inbox shows messages, handle valid and invalid form submissions

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -19,6 +19,16 @@ const getCookie = (name) => {
 };
 
 /**
+ * A recursive function to return an 'email' element
+ * @param {*} element: an 'email' element or an element contained by an 'email' element.
+ * @returns an email element
+ */
+const getEmailDiv = (element) =>
+  element.classList.contains("email")
+    ? element
+    : getEmailDiv(element.parentElement);
+
+/**
  * Get an object of all the named fields in an HTML form
  * @param {*} form DOM element
  */
@@ -30,6 +40,38 @@ const getFields = (form) => {
   const fields = rawFields.reduce((o, key) => ({ ...o, [key.name]: key }), {});
   return fields;
 };
+
+/**
+ *
+ * @param {*} fields
+ * @returns Object with the named fields as keys and the field values as the values
+ */
+const getFormValues = (fields) =>
+  Object.fromEntries(
+    Object.entries(fields).map(([key, val]) => [key, val.value])
+  );
+
+/**
+ *
+ * @param {*} values: Form field values
+ * @returns a promise
+ */
+const postEmail = (values) =>
+  fetch("/emails", {
+    method: "POST",
+    headers: { "X-CSRFToken": getCookie("csrftoken") },
+    body: JSON.stringify(values),
+  }).then((response) => {
+    if (response.ok) return response.json();
+    else {
+      // create error object and reject if not a 2xx response code
+      let err = new Error(response.statusText);
+      err.response = response;
+      err.status = response.status;
+      err.message = response.statusText;
+      throw err;
+    }
+  });
 
 /**
  * Remove invalid messages from the form.
@@ -92,17 +134,14 @@ const emailAddressValidator = (field) => {
  * Validate for an input length of minimum of 1.
  * @param {*} field DOM form field
  */
-const requiredFieldValidator = field => {
+const requiredFieldValidator = (field) => {
   if (field.value <= 0) {
-    const errorMessage = errorFeedbackWriter(
-      field,
-      "This field is required."
-    )
-    field.parentElement.append(errorMessage)
+    const errorMessage = errorFeedbackWriter(field, "This field is required.");
+    field.parentElement.append(errorMessage);
   } else {
     field.classList.add("is-valid");
   }
-}
+};
 
 // DOM ELEMENT WRITERS
 
@@ -120,6 +159,37 @@ const errorFeedbackWriter = (field, message) => {
   errorMessage.classList.add("invalid-feedback");
   errorMessage.innerText = message;
   return errorMessage;
+};
+
+const inboxMessageWriter = (container, email) => {
+  console.log(email);
+  const emailDiv = document.createElement("div");
+  emailDiv.classList.add("email");
+  emailDiv.dataset.id = email.id;
+  emailDiv.addEventListener("click", emailClickEvent);
+  if (email.read) emailDiv.classList.add("bg-light");
+
+  const leftDiv = document.createElement("div");
+  const rightDiv = document.createElement("div");
+
+  const senderSpan = document.createElement("span");
+  senderSpan.innerText = email.sender;
+
+  const subjectSpan = document.createElement("span");
+  subjectSpan.innerText = email.subject;
+
+  rightDiv.innerText = email.timestamp;
+
+  leftDiv.appendChild(senderSpan);
+  leftDiv.appendChild(subjectSpan);
+
+  emailDiv.appendChild(leftDiv);
+  emailDiv.appendChild(rightDiv);
+  container.appendChild(emailDiv);
+};
+
+const flash = (message, tag) => {
+  console.log(`${tag}: ${message}`);
 };
 
 // EVENT HANDLERS
@@ -141,8 +211,22 @@ const composeSubmitHander = (event) => {
   requiredFieldValidator(fields.recipients);
   emailAddressValidator(fields.recipients);
 
-  if (formIsValid()) console.log(fields);
-  else return;
+  if (formIsValid()) {
+    // Create an array of form values
+    const values = getFormValues(fields);
+    postEmail(values)
+      .then((data) => flash(data.message, "success"))
+      .catch((error) => {
+        if (error.status === 400)
+          flash("User does not exist in system.", "danger");
+        else console.log(error);
+      });
+  } else return;
+};
+
+const emailClickEvent = (event) => {
+  const target = getEmailDiv(event.target);
+  console.log(target.dataset.id);
 };
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -185,8 +269,18 @@ function load_mailbox(mailbox) {
   document.querySelector("#emails-view").style.display = "block";
   document.querySelector("#compose-view").style.display = "none";
 
+  const content = document.querySelector("#emails-view");
   // Show the mailbox name
-  document.querySelector("#emails-view").innerHTML = `<h3>${
+  content.innerHTML = `<h3>${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }</h3>`;
+
+  // get the data
+  fetch("/emails/inbox")
+    .then((response) => response.json())
+    .then((emails) => {
+      emails.forEach((email) => inboxMessageWriter(content, email));
+
+      // ... do something else with emails ...
+    });
 }
